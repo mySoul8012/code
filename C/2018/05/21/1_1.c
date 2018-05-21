9481cd798850372526583e4d9a04bbb86538b86c
mingming <mingming@mingming.email>
2018-05-21 22:28:33 +0800

    增加一个网页，以及一个C

---
+++ C/2018/05/21/1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	f(void);
+
+	return 0;
+}
+
+void f(void)
+{
+	static count;
+
+	count++;
+
+	if
+} 
---
+++ C/2018/05/21/README.md
@@ -0,0 +1,3 @@
+# 输出全年的日历
+https://www.iming.info/2018/05/21/857.html
+
