f2e91f9649b9e88f7f40bfe8e9e8efb087d5916b
小明明 <mingming@mingming.email>
2018-07-19 03:44:10 +0800

    js中的Java式继承

---
+++ web/07/19/README.md
@@ -0,0 +1,3 @@
+# 这里是实现一个复数的类
+使用的是js实现的
+原文地址 https://www.iming.info/archives/471.html
---
+++ web/07/19/comple.js
@@ -0,0 +1,14 @@
+/*
+ * Comple.js
+ * 这个文件定义了Complex类，是用来描述复数
+ * 复数是实数和虚数的和，并且虚数i是-1的平方根
+ */
+
+/*
+ * 这个构造函数为它创建的每个实例定义了字段r和i
+ * 这两个字段分别保存复数的实部和虚部
+ * 它们是对象的状态
+ */
+function Complex(real, imaginary) {
+	if (isNaN(real) || isNaN(imaginary) )
+}
